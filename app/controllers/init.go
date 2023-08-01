4de8f5fd2f3dbdda165a0554ff46c7f3386e2e79
Alexander Loshakov <alevas556@icloud.com>
2023-08-01 23:47:19 +0300

    Вывод ошибок подключения к БД

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"database/sql"
+	"log"
 
 	"github.com/revel/revel"
 )
@@ -10,15 +11,22 @@ var db *sql.DB
 
 // Инициализация Подключения к Базе данных
 func InitDB() {
-	// Берем "db.url" из app.conf (или передаем как env-переменную для докера)
+	// Берем "db.url" из app.conf (или передаем как env-переменную - для докера)
 	connstring := revel.Config.StringDefault("db.url", "")
-	revel.AppLog.Infof("DB connstring: ", connstring)
+	revel.AppLog.Infof("DB connection string: " + connstring)
 
 	var err error
 	db, err = sql.Open("postgres", connstring)
+
+	// Ошибка параметров подключения к БД
 	if err != nil {
-		revel.AppLog.Infof("DB Error", err)
+		log.Fatal("Invalid DB config:", err)
 	}
+	// Отсутствие подключения к БД (sql.Open НЕ гарантирует установку соединения)
+	if err = db.Ping(); err != nil {
+		log.Fatal("DB unreachable:", err)
+	}
+
 	revel.AppLog.Infof("DB Connected")
 }
 
