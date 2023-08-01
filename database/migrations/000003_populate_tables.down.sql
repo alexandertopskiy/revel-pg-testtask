d604c3072d2459c45a5e58f4c9f1f75c77069cd0
Alexander Loshakov <alevas556@icloud.com>
2023-08-01 07:54:23 +0300

    Миграция для добавления данных в таблицу

---
+++ database/migrations/000003_populate_tables.down.sql
@@ -0,0 +1,17 @@
+-- Удаляем данные в таблицу Книги-Авторы
+DELETE FROM book_author;
+
+-- Удаляем данные в таблицу Книги-Издательства
+DELETE FROM book_publisher;
+
+-- Удаляем данные в таблицу Авторы-Издательства
+DELETE FROM author_publisher;
+
+-- Удаляем книги
+DELETE FROM book;
+
+-- Удаляем авторов
+DELETE FROM author;
+
+-- Удаляем издательства
+DELETE FROM publisher;
